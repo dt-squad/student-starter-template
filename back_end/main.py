0f8ab5c1e5e0dfefc0ac419df6b1901e1c5f30e4
Ivan Shaw <ivan.shaw695@gmail.com>
2026-04-20 15:39:53 +0100

    basic vue and fastapi

---
+++ back_end/main.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from .models import Message
+
+app = FastAPI()
+
+
+@app.get("/api/hello", response_model=Message)
+def hello() -> Message:
+    return Message(text="Hello from FastAPI")
+
+
+static_dir = Path(__file__).parent / "static"
+if static_dir.is_dir():
+    app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
