a4f6460c9d4292703d2d49f213b8ee744460c40e
antony <antonychalkley4728@gmail.com>
2026-08-17 16:36:22 +0000

    Adjusted the routes for all jobs to include try and excepts

--- back_end/app/api/jobs/routes.py
+++ back_end/app/api/jobs/routes.py
@@ -34,11 +34,12 @@ class Read_Job_Request(BaseModel):
 
 @router.post("/read")
 def read_job(request:Read_Job_Request):
-    if request.id:
-        job = Job_Service.read_job(request.id)
-        return job
-    else:
-        return "Job not found."
+    try:
+        if request.id:
+            job = job_service_instance.read_job(request.id)
+            return dict(job=job, message="Job found", rc=0)
+    except:
+        return dict(message="Job not found.", rc=16)
 
 
 class Read_Job_All_Request(BaseModel):
@@ -49,10 +50,13 @@ class Read_Job_All_Request(BaseModel):
 
 @router.post("/read_all")
 def read_job_all(request: Read_Job_All_Request):
-    jobs = job_service_instance.read_job_all(
-        request.job_number, request.address, request.postcode, request.complaint_id
-    )
-    return jobs
+    try:
+        jobs = job_service_instance.read_job_all(
+            request.job_number, request.address, request.postcode, request.complaint_id
+        )
+        return dict(jobs=jobs, message="Jobs loaded successfully.", rc=0)
+    except:
+        return dict(message="Failed to load jobs", rc=16)
 
 
 class Update_Job_Request(BaseModel):
@@ -64,14 +68,18 @@ class Update_Job_Request(BaseModel):
 
 @router.post("/update")
 def update_job(request: Update_Job_Request):
-    result = job_service_instance.update_job(
-        id=request.id,
-        job_number=request.job_number,
-        address=request.address,
-        postcode=request.postcode,
-        complaint_id=request.complaint_id
-    )
-    return {"success": result}
+    try:
+        result = job_service_instance.update_job(
+            id=request.id,
+            job_number=request.job_number,
+            address=request.address,
+            postcode=request.postcode,
+            complaint_id=request.complaint_id
+        )
+        return dict(result=result, message="Job updated successfully", rc=0)
+    except:
+        return dict(message="Job failed to update", rc=16)
+
 
 
 class Delete_Job_Request(BaseModel):
@@ -79,8 +87,11 @@ class Delete_Job_Request(BaseModel):
 
 @router.post("/delete")
 def delete_job(request:Delete_Job_Request):
-    job = job_service_instance.delete_job(request.id)
-    return job
+    try:
+        job = job_service_instance.delete_job(request.id)
+        return dict(job=job, message="Job Deleted successfully", rc=0)
+    except:
+        return dict(message="Failed deleting job", rc=16)
 
 
 
@@ -90,8 +101,11 @@ class Create_Job_Stage_Request(BaseModel):
 
 @router.post("/create_stage")
 def create_job_stage(request:Create_Job_Stage_Request):
-    job = Job_Service.create_job_stage(request.title, request.for_scaffold)
-    return job
+    try:
+        job_stage = job_service_instance.create_job_stage(request.title, request.for_scaffold)
+        return dict(job_stage=job_stage, message="Job stage created successfully", rc=0)
+    except:
+        return dict(message="Failed to create job stage", rc=16)
 
 
 class Read_Job_Stage_Request(BaseModel):
@@ -99,8 +113,11 @@ class Read_Job_Stage_Request(BaseModel):
 
 @router.post("/read_stage")
 def read_job_stage(request:Read_Job_Stage_Request):
-    job = Job_Service.read_job_stage(request.id)
-    return job
+    try:
+        job_stage = job_service_instance.read_job_stage(request.id)
+        return dict(job_stage=job_stage, message="Job stage loaded successfully", rc=0)
+    except:
+        return dict(message="Failed to load job stage", rc=16)
 
 
 class Read_Job_Stage_All_Request(BaseModel):
@@ -109,8 +126,11 @@ class Read_Job_Stage_All_Request(BaseModel):
 
 @router.post("/read_stage_all")
 def read_job_stage_all(request:Read_Job_Stage_All_Request):
-    job = Job_Service.read_job_stage_all(request.title, request.for_scaffold)
-    return job
+    try:
+        job_stage_all = job_service_instance.read_job_stage_all(request.title, request.for_scaffold)
+        return dict(job_stage=job_stage_all, message="All job stages loaded successfully", rc=0)
+    except:
+        return dict(message="Failed to load all job stages", rc=16)
 
 
 class Update_Job_Stage_Request(BaseModel):
@@ -120,8 +140,11 @@ class Update_Job_Stage_Request(BaseModel):
 
 @router.post("/update_stage")
 def update_stage_job(request:Update_Job_Stage_Request):
-    job = Job_Service.update_job_stage(request.id, request.title, request.for_scaffold)
-    return job
+    try:
+        job_stage = job_service_instance.update_job_stage(request.id, request.title, request.for_scaffold)
+        return dict(job_stage=job_stage, message="Job stage updated successfully", rc=0)
+    except:
+        return dict(message="Failed to update job stage", rc=16)
 
 
 class Delete_Job_Stage_Request(BaseModel):
@@ -129,8 +152,11 @@ class Delete_Job_Stage_Request(BaseModel):
 
 @router.post("/delete_stage")
 def delete_job(request:Delete_Job_Stage_Request):
-    job = Job_Service.delete_job_stage(request.id)
-    return job
+    try:
+        job_stage = job_service_instance.delete_job_stage(request.id)
+        return dict(job_stage=job_stage, message="Job stage deleted successfully", rc=0)
+    except:
+        return dict(message="Failed to delete job stage", rc=16)
 
 
 
@@ -141,8 +167,11 @@ class Create_Job_Stage_History_Request(BaseModel):
 
 @router.post("/create_stage_history")
 def create_job_stage_history(request:Create_Job_Stage_History_Request):
-    job = Job_Service.create_job_stage_history(request.job_stage_history_id, request.job_id, request.stages_id, request.date)
-    return job
+    try:
+        job_stage_history = job_service_instance.create_job_stage_history(request.job_stage_history_id, request.job_id, request.stages_id, request.date)
+        return dict(job_stage_history=job_stage_history, message="Job stage history created successfully", rc=0)
+    except:
+        return dict(message="Failed to create job stage history", rc=16)
 
 
 class Read_Job_Stage_History_Request(BaseModel):
@@ -150,8 +179,11 @@ class Read_Job_Stage_History_Request(BaseModel):
 
 @router.post("/read_stage_history")
 def read_job_stage_history(request:Read_Job_Stage_History_Request):
-    job = Job_Service.read_job_stage_history(request.id)
-    return job
+    try:
+        job_stage_history = job_service_instance.read_job_stage_history(request.id)
+        return dict(job_stage_history=job_stage_history, message="Job stage history loaded successfully", rc=0)
+    except:
+        return dict(message="Failed to load job stage history", rc=16)
 
 
 class Read_Job_Stage_History_All_Request(BaseModel):
@@ -161,8 +193,11 @@ class Read_Job_Stage_History_All_Request(BaseModel):
 
 @router.post("/read_all_stage_history")
 def read_job_stage_history_all(request:Read_Job_Stage_History_All_Request):
-    job = Job_Service.read_job_stage_history_all(request.job_id, request.stages_id, request.date)
-    return job
+    try:
+        job_stage_history_all = job_service_instance.read_job_stage_history_all(request.job_id, request.stages_id, request.date)
+        return dict(job_stage_history=job_stage_history_all, message="All job stage history loaded successfully", rc=0)
+    except:
+        return dict(message="Failed to load all job stage history", rc=16)
 
 
 class Update_Job_Stage_History_Request(BaseModel):
@@ -173,8 +208,11 @@ class Update_Job_Stage_History_Request(BaseModel):
 
 @router.post("/update_stage_history")
 def update_job_stage_history(request:Update_Job_Stage_History_Request):
-    job = Job_Service.update_job_stage_history(request.id, request.job_number, request.address, request.postcode, request.complaint_id)
-    return job
+    try:
+        job_stage_history_all = job_service_instance.update_job_stage_history(request.id, request.job_number, request.address, request.postcode, request.complaint_id)
+        return dict(job_stage_history=job_stage_history_all, message="Job stage history updated successfully", rc=0)
+    except:
+        return dict(message="Failed to update job stage history", rc=16)
 
 
 class Delete_Job_Stage_History_Request(BaseModel):
@@ -182,8 +220,11 @@ class Delete_Job_Stage_History_Request(BaseModel):
 
 @router.post("/delete_stage_history")
 def delete_job_stage_history(request:Delete_Job_Stage_History_Request):
-    job = Job_Service.delete_job_stage_history(request.id)
-    return job
+    try:
+        job_stage_history_all = job_service_instance.delete_job_stage_history(request.id)
+        return dict(job_stage_history=job_stage_history_all, message="Job stage history deleted successfully", rc=0)
+    except:
+        return dict(message="Failed to deleted job stage history", rc=16)
 
 
 
--- front_end/src/main.js
+++ front_end/src/main.js
@@ -10,3 +10,7 @@ app.mount('#app')
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap-icons/font/bootstrap-icons.css";
+
+function toast(message){
+    
+}
